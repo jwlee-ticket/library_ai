f68ccc7157594e7aa5b260d74b62c1d019f4e8c0
jwlee07 <apzk21413@gmail.com>
2025-12-19 16:03:55 +0900

    feat: 로그인 로직 수정

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class EmailAuthenticationForm(AuthenticationForm):
@@ -24,4 +27,24 @@ class EmailAuthenticationForm(AuthenticationForm):
         'invalid_login': '이메일 또는 비밀번호가 올바르지 않습니다.',
         'inactive': '이 계정은 비활성화되어 있습니다.',
     }
+    
+    def clean(self):
+        """이메일을 username으로 변환하여 인증"""
+        email = self.cleaned_data.get('username')  # 폼에서는 username 필드에 이메일 입력
+        password = self.cleaned_data.get('password')
+        
+        if email and password:
+            # 이메일로 사용자 찾기
+            try:
+                user = User.objects.get(email=email)
+                # username을 실제 username으로 설정
+                self.cleaned_data['username'] = user.username
+            except User.DoesNotExist:
+                # 사용자가 없으면 기본 인증 로직에서 처리하도록
+                pass
+            
+            # 부모 클래스의 clean() 호출하여 인증 수행
+            return super().clean()
+        
+        return self.cleaned_data
 
